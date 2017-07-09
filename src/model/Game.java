88fc5831921520ff32bcaa072f92f2f2fb4c7d7f
juclopezso <juclopezso@unal.edu.co>
2017-07-08 20:31:27 -0500

    Update Game.java

--- src/model/Game.java
+++ src/model/Game.java
@@ -22,31 +22,13 @@ public class Game {
     	boards.add(board);
     	return boards;
     }
-    
-    //Print the field Symbol
-    public void printField() {
-         for(int y=0; y<7 ;y++){
-			for(int x=0; x<7 ; x++) {
-                System.out.print(getFieldOwner(x, y) + " ");
-            }
-            System.out.println();
-        }
-    }
-    
-    public void printAccess() {
-    	 for(int y=0; y<7 ;y++){
- 			for(int x=0; x<7 ; x++) {
- 				System.out.print(getFieldAccessible(x, y) + " ");
-            }
-            System.out.println();
-        }
-    }
+   
     //Condition to finish the game
     public boolean getDidSomeoneWin() {
         return didSomeoneWin;
     }
     
-    //Condition to game´s end
+    //Condition to gameÂ´s end
     public boolean isGameOver() {
         int score = board.evaluateBoard();
         //printAccess();
